20831f13f9864d0f1a0267863e1e3e33c52a2e42
Hqen <36402286+Hqen@users.noreply.github.com>
2018-09-28 20:22:13 +0500

    Add files via upload

---
+++ eds.loc/Mobile/Mobile/Mobile.Android/BaseUrl_Android.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Mobile.Droid;
+
+[assembly: Dependency(typeof(BaseUrl_Android))]
+namespace Mobile.Droid    // пространство имен может отличаться
+{
+    public class BaseUrl_Android : IBaseUrl
+    {
+        public string Get()
+        {
+            return "file:///android_asset/";
+        }
+    }
+}
---
+++ eds.loc/Mobile/Mobile/Mobile/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var urlSource = new UrlWebViewSource
+            {
+                Url = System.IO.Path.Combine(DependencyService.Get<IBaseUrl>().Get(), "index.html")
+            };
+            var webView = new WebView
+            {
+                Source = urlSource
+            };
+            Content = webView;
+        }
+    }
+    public interface IBaseUrl { string Get(); }
+}
